f551af7a25c4b3119aa38758683108b2c229e79d
evhook <anonymous1986222@gmail.com>
2026-03-29 11:45:36 +0530

    Refactor Round Robin scheduling algorithm for improved readability and consistency

--- RoundRobin.c
+++ RoundRobin.c
@@ -3,60 +3,58 @@
 struct process{
     int pid;
     int btime;
+    int tatime;
     int wtime;
-    int tatime; 
-    int atime;
     int ctime;
     int rtime;
+    int atime;
 };
 typedef struct process *PROCESS;
 void main(){
-    int n;
-    printf("Enter number of process : ");
+    int n,completed = 0,t=0;
+    printf("Enter number of process: ");
     scanf("%d",&n);
     PROCESS p = (PROCESS)malloc(n*sizeof(struct process));
     for(int i=0;i<n;i++){
-        p[i].pid= i;
-        printf("Enter burst time for the process P%d: ",i);
+        p[i].pid = i+1;
+        printf("Enter burst time of the Process P%d\n",i);
         scanf("%d",&p[i].btime);
-        printf("Enter arrival time for the process P%d: ",i);
-        scanf("%d",&p[i].atime); 
+        printf("Enter arrival time of the Process P%d\n",i);
+        scanf("%d",&p[i].atime);
         p[i].rtime = p[i].btime;
-        p[i].ctime = p[i].tatime = p[i].wtime = 0;
+        p[i].ctime = p[i].wtime = p[i].tatime = 0;
     }
-    int completed=0,t=0;
-    float awtime = 0.0;
-    float awtatime = 0.0; 
-    int tq;
-    printf("Enter time quantum: ");
+    int tq=0;
+    printf("Enter time quantum for the problem: ");
     scanf("%d",&tq);
     while(completed!=n){
         int done=1;
         for(int i=0;i<n;i++){
             if(p[i].atime<=t&&p[i].rtime>0){
-                done = 0;
-                printf("P%d ",p[i].pid);
-                if(p[i].rtime > tq){
+                done=0;
+                if(p[i].rtime>tq){
                     t+=tq;
-                    p[i].rtime -=tq;
+                    p[i].rtime-=tq;
                 }else{
-                    tq+=p[i].rtime;
+                    t+=p[i].rtime;
                     p[i].rtime = 0;
-                    completed++;
+                    p[i].ctime=t;
                     p[i].tatime = p[i].ctime - p[i].atime;
                     p[i].wtime = p[i].tatime - p[i].btime;
-                }
+                    completed++;
+            }
             }
         }
-        if(done)
-            t++;
+    if(done) t++;
     }
-    printf("\nProcess\tBurst Time\tTurn Around Time\tWaiting Time\tArrival Time\tCompletion Time\n");
+
+    float awtime = 0.0,atatime=0.0;
+    printf("\nProcess\tBurst Time\tTurn Around Time\tWaiting Time\n");
     for(int i=0;i<n;i++){
-        printf("P%d\t%d\t\t%d\t\t\t%d\t\t%d\t\t\n",p[i].pid,p[i].btime,p[i].tatime,p[i].wtime,p[i].atime,p[i].ctime);
-        awtime +=  p[i].wtime;
-        awtatime += p[i].tatime;
+        printf("\n%d\t%d\t\t%d\t\t\t%d\n",p[i].pid,p[i].btime,p[i].tatime,p[i].wtime);
+        awtime += p[i].wtime;
+        atatime += p[i].tatime;
     }
-    printf("Average waiting time : %.2f\n",awtime/n);
-    printf("Average turn around time : %.2f\n",awtatime/n);
+    printf("The Average Waiting Time : %.2f\n",awtime/n);
+    printf("The Average Turn Around Time : %.2f\n",atatime/n);
 }
