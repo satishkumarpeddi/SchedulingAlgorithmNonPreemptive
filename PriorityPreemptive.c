fc6c86e3f3a2d9f2bfa2e92f5a19c859998f7548
evhook <anonymous1986222@gmail.com>
2026-03-29 11:40:06 +0530

    Refactor process structure and improve output formatting in PriorityNonPreemptive.c

--- PriorityPreemptive.c
+++ PriorityPreemptive.c
@@ -3,64 +3,61 @@
 struct process{
     int pid;
     int btime;
-    int wtime;
     int tatime;
+    int wtime;
     int priority;
-    int atime;
     int ctime;
     int rtime;
+    int atime;
 };
 typedef struct process *PROCESS;
 void main(){
-    int n;
-    printf("Enter number of process : ");
+    int n,completed = 0,t=0;
+    printf("Enter number of process: ");
     scanf("%d",&n);
     PROCESS p = (PROCESS)malloc(n*sizeof(struct process));
     for(int i=0;i<n;i++){
-        p[i].pid= i;
-        printf("Enter burst time for the process P%d: ",i);
+        p[i].pid = i+1;
+        printf("Enter burst time of the Process P%d\n",i);
         scanf("%d",&p[i].btime);
-        printf("Enter arrival time for the process P%d: ",i);
+        printf("Enter arrival time of the Process P%d\n",i);
         scanf("%d",&p[i].atime);
-        printf("Enter priorty for the process P%d: ",i);
+        printf("Enter priority time of the Process P%d\n",i);
         scanf("%d",&p[i].priority);
         p[i].rtime = p[i].btime;
-        p[i].ctime = p[i].tatime = p[i].wtime = 0;
+        p[i].ctime = p[i].wtime = p[i].tatime = 0;
     }
-    int completed=0,t=0;
-    float awtime = 0.0;
-    float awtatime = 0.0; 
     while(completed!=n){
-        int highest_priority = 1000000;
+        int highest_priority = 100000;
         int idx = -1;
         for(int i=0;i<n;i++){
             if(p[i].atime<=t&&p[i].rtime>0){
-                if(p[i].priority<highest_priority){
-                    highest_priority=p[i].priority;
-                    idx =i;
+                    if(p[i].priority<highest_priority){
+                    highest_priority = p[i].priority;
+                    idx = i;
                 }
             }
         }
-            if(idx == -1){
-                t++;
-                continue;
-            }
-            printf("P%d --> ",p[idx].pid);
-            p[idx].rtime--;
+        if(idx==-1){
             t++;
-            if(p[idx].rtime==0){
-                completed++;
-                p[idx].ctime = t;
-                p[idx].tatime = p[idx].ctime - p[idx].atime;
-                p[idx].wtime = p[idx].tatime - p[idx].btime;
-            }
+            continue;
+        } 
+        p[idx].rtime--;
+        t++;
+        if(p[idx].rtime == 0){
+            p[idx].ctime = t;
+            p[idx].tatime = p[idx].ctime - p[idx].atime;
+            p[idx].wtime = p[idx].tatime - p[idx].btime;
+            completed++;
+        }
     }
-    printf("\nProcess\tBurst Time\tTurn Around Time\tWaiting Time\tArrival Time\tCompletion Time\n");
+    float awtime = 0.0,atatime=0.0;
+    printf("\nProcess\tBurst Time\tTurn Around Time\tWaiting Time\n");
     for(int i=0;i<n;i++){
-        printf("P%d\t%d\t\t%d\t\t\t%d\t\t%d\t\t\n",p[i].pid,p[i].btime,p[i].tatime,p[i].wtime,p[i].atime,p[i].ctime);
-        awtime +=  p[i].wtime;
-        awtatime += p[i].tatime;
+        printf("\n%d\t%d\t\t%d\t\t\t%d\n",p[i].pid,p[i].btime,p[i].tatime,p[i].wtime);
+        awtime += p[i].wtime;
+        atatime += p[i].tatime;
     }
-    printf("Average waiting time : %.2f\n",awtime/n);
-    printf("Average turn around time : %.2f\n",awtatime/n);
+    printf("The Average Waiting Time : %.2f\n",awtime/n);
+    printf("The Average Turn Around Time : %.2f\n",atatime/n);
 }
